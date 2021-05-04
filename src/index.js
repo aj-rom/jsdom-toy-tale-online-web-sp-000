b31745f23fb5d6d3732ef13aa4395b95d875f19f
A.J. Romaniello <ajiellodev@gmail.com>
2021-05-04 10:00:18 -0600

    commit tree 1a1c6db9ac200d6d6b81a89118c88412efce9a42

--- src/index.js
+++ src/index.js
@@ -13,3 +13,45 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+let toyCollection = document.getElementById('toy-collection')
+const allToyHeader = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  }
+}
+
+function fetchToys() {
+  return fetch('http://localhost:3000/toys', allToyHeader)
+      .then(resp => resp.json())
+      .then(e => renderToys(e))
+      .catch(e => {
+        console.log("An error has occurred while fetching toys.")
+      })
+}
+
+function renderToys(toys) {
+    toys.forEach(toy => {
+      let div = document.createElement('div')
+      div.class = 'card'
+
+      let h2 = document.createElement('h2')
+      h2.textContent = toy.name
+
+      let img = document.createElement('img')
+      img.src = toy.url
+
+      let p = document.createElement('p')
+      p.textContent = `${toy.likes} Likes`
+
+      let button = document.createElement('button');
+      button.class = 'like-btn'
+      button.innerText = 'Like <3'
+
+      div.append(h2, img, p, button)
+      toyCollection.appendChild(div)
+    })
+}
+
+fetchToys()
